cc254df41c7b73505699d4f7267c473e2f6e6135
tmgAzit <azit.ghising@gmail.com>
2023-11-23 19:39:07 +1030

    Basic 3d animation sphere with color change is complete.

---
+++ main.js
@@ -0,0 +1,107 @@
+import * as THREE from 'three';
+import './style.css';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import gsap from 'gsap';
+// Scene
+const scene = new THREE.Scene();
+
+// Sphere/object
+const geometry = new THREE.SphereGeometry(3, 70, 70); //shape
+
+const material = new THREE.MeshStandardMaterial({ color: '#00ff83' });
+
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+// Light
+const light = new THREE.PointLight(0xffffff, 350, 20);
+light.position.set(0, 10, 10);
+scene.add(light);
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+  45,
+  sizes.width / sizes.height,
+  0.1,
+  1000
+);
+camera.position.z = 30;
+scene.add(camera);
+
+// Renderer
+const canvas = document.querySelector('.webgl');
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.enableZoom = false;
+controls.autoRotate = true;
+controls.autoRotateSpeed = 4;
+
+// Resize
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(sizes.width, sizes.height);
+});
+
+const loop = () => {
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(loop);
+};
+
+loop();
+
+// Timeline magic
+const tl = gsap.timeline({ defaults: { duration: 1 } });
+tl.fromTo(
+  mesh.scale,
+  { x: 0, y: 0, z: 0 },
+  {
+    x: 1,
+    y: 1,
+    z: 1,
+  }
+);
+tl.fromTo('nav', { y: '-100%' }, { y: '0%' });
+tl.fromTo('.title', { opacity: 0 }, { opacity: 1 });
+
+// Mouse Animation Color
+let mouseDown = false;
+let rgb = [];
+window.addEventListener('mousedown', () => (mouseDown = true));
+window.addEventListener('mouseup', () => (mouseDown = false));
+
+window.addEventListener('mousemove', (e) => {
+  if (mouseDown) {
+    rgb = [
+      Math.round((e.pageX / sizes.width) * 255),
+      Math.round((e.pageY / sizes.height) * 255),
+      150,
+    ];
+    // console.log(rgb);
+    // lets animate
+    let newColor = new THREE.Color(`rgb(${rgb.join(',')})`);
+    console.log(newColor);
+    gsap.to(mesh.material.color, {
+      r: newColor.r,
+      g: newColor.g,
+      b: newColor.b,
+    });
+  }
+});
